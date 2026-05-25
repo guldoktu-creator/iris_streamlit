b5f5ba200e05925d2f1724d2b8df3f830b15eed1
guldo <guldoktu@gmail.com>
2026-05-25 11:39:27 +0900

    1st commit

---
+++ model.py
@@ -0,0 +1,21 @@
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+import pickle
+
+# 1. Irisデータセットをロード
+iris = datasets.load_iris()
+
+# 2. データをDataFrameに変換
+features = pd.DataFrame(iris['data'], columns=iris['feature_names'])
+target = iris['target']
+
+# 3. モデルを作成
+model = RandomForestClassifier()
+
+# 4. 学習させる
+model.fit(features, target)
+
+# 5. 保存
+with open('models/model_iris.pkl', 'wb') as f:
+    pickle.dump(model, f)
